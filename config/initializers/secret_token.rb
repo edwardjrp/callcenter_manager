04a0bd0c1cddc677ffa34478f30d2947e41d86cf
Radhames Brito <rbritom@gmail.com>
2012-06-23 09:37:18 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kapiqua25::Application.config.secret_token = '234c3ab791262e2dca47d8f72a57cb029ca5e821a9d33e96c252423894143b4c71f9ee0530db783306b5a4a8707239dff50ee9c95c2159bc8871dfc5ebbf2a35'
