acc73f0929f67fe2fc506b4be3aff79e37f3bd33
Radhames Brito <rbritom@gmail.com>
2012-06-26 13:54:02 -0400

    stopped for speech

--- spec/requests/home/home_indices_spec.rb
+++ spec/requests/home/home_indices_spec.rb
@@ -7,10 +7,9 @@ describe "Home::Indices" do
       before(:each) do
         login(FactoryGirl.create(:user))
         @client = FactoryGirl.create(:client, first_name: 'tester')
-        @phone = FactoryGirl.create :phone, client: @client
-        10.times{FactoryGirl.create(:client)}
+        @phone = FactoryGirl.create :phone, client: @client, number: '8095551234', ext: nil
+        # 10.times{FactoryGirl.create(:client)}
         visit root_path 
-        
       end
       it "should render the main page" do
         page.should have_content('Clientes')
@@ -20,11 +19,30 @@ describe "Home::Indices" do
          page.should have_css('#client_search')
        end
        
-       it "should find the users in the list", js: true do
-         fill_in "client_search_phone", with: '8095551234'
-         within('#client_listing'){page.should have_content('tester')}
-       end
     end
+    
+    context "When search for a client" do
+       before(:each)do
+         Capybara.current_driver = :selenium_chrome
+         login(FactoryGirl.create(:user))
+         @client = FactoryGirl.create(:client, first_name: 'tester')
+         @phone = FactoryGirl.create :phone, client: @client, number: '8095551234', ext: '99'
+         # 10.times{FactoryGirl.create(:client)}
+         visit root_path
+       end
+
+       after(:each)do
+         Capybara.use_default_driver
+       end
+       it "should find the users in the list", js: true do
+         selector = '.ui-menu-item a:first'
+         fill_in "client_search_phone", with: '8095551234'
+         sleep(3)
+         page.has_content? selector
+         page.execute_script " $('#{selector}').trigger(\"mouseenter\").click();"
+         within('#client_search_first_name'){page.should have_content('tester')}
+       end
+     end
     
     context 'when user is not logged' do
       it "should redirect_to the login" do
