d624eaa3118fe7af5602c9cf39502a1e5e449a67
Radhames Brito <rbritom@gmail.com>
2012-07-15 23:57:09 -0400

    cart to json done

--- kapiquajs/app.js
+++ kapiquajs/app.js
@@ -37,26 +37,41 @@ app.post('/cart_products', function(req, res){
   res.contentType('application/json');
   // console.log(util.inspect(res, true, 1));
   cart_product = req.body
-  console.log(cart_product.cart)
-  console.log(cart_product.product.id)
-  console.log(cart_product.bind_id)
-  console.log(cart_product.options)
-  console.log(cart_product.quantity)  
-  console.log(cart_product.updated_at)
   // INSERT INTO cart_products( "cart_id","quantity","product_id", "bind_id","options", "created_at", "updated_at" ) values ($1, $2, $3, $4, $5, $6, $7)  RETURNING "id"',[cart_product['cart_id'],cart_product['quantity'],cart_product['product_id'],cart_product['bind_id'],cart_product['options'], new Date(), new Date()]
   var save_cart_product = client.query('INSERT INTO cart_products( "cart_id","quantity","product_id", "bind_id","options", "created_at", "updated_at" ) values ($1, $2, $3, $4, $5, $6, $7)  RETURNING "id"',
               [cart_product.cart,cart_product.quantity,cart_product.product.id,cart_product.bind_id,cart_product.options, new Date(), new Date() ]);
+              
+              
   save_cart_product.on('end', function(cart_product_result) {
-      var get_current_cart = client.query('SELECT * FROM carts WHERE "id" = $1 LIMIT 1', [cart_product.cart]);
-      get_current_cart.on('row',function(current_cart){
-         res.send(current_cart);
-      })
-      get_current_cart.on('error',function(current_cart_error){
+    
+    var get_current_cart = client.query('SELECT * FROM carts WHERE "id" = $1 LIMIT 1', [cart_product.cart]);
+    
+    get_current_cart.on('row',function(current_cart){
+      
+      var get_cart_products = client.query('SELECT * FROM cart_products WHERE cart_id = $1', [current_cart.id])
+      
+      var cart_products = [];
+      
+      get_cart_products.on('row',function(current_cart_product){
+        console.log(current_cart_product.product_id)
+        var get_current_product = client.query('SELECT * FROM products WHERE id = $1 LIMIT 1', [current_cart_product.product_id]);
+        
+        get_current_product.on('row', function(product){
+          current_cart_product.product = product;
+          cart_products.push(current_cart_product)
+        });
+        get_current_product.on('end', function(productResult){
+          current_cart.cart_products = cart_products;
+          res.send(current_cart);
+        });
+      });
+    })
+    get_current_cart.on('error',function(current_cart_error){
          res.send(current_cart_error);
-      })
+    })
   });
-  save_cart_product.on('error', function(cart_product__error) {
-     res.send(JSON.stringify(cart_product__error.message));
+  save_cart_product.on('error', function(cart_product_error) {
+     res.send(JSON.stringify(cart_product_error.message));
   });
 })
 
