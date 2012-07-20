2f7757e84d1b1a7218de15882fd8015095552449
Radhames Brito <rbritom@gmail.com>
2012-07-19 23:06:30 -0400

    more fixes to config

--- deploy_configs/kapiqua_unicorn.rb
+++ deploy_configs/kapiqua_unicorn.rb
@@ -1,5 +1,5 @@
 working_directory "/var/www/kapiqua25/current"
-pid "/var/www/kapiqua25/current/pids/unicorn.pid"
+pid "/var/www/kapiqua25/current/tmp/pids/unicorn.pid"
 stderr_path "/var/www/kapiqua25/current/log/unicorn.log"
 stdout_path "/var/www/kapiqua25/current/log/unicorn.log"
 
