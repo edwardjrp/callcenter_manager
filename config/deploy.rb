5046ddf4057c30656ac8ca8e58bbbe8304280f3d
Radhames Brito <rbritom@gmail.com>
2012-07-19 22:17:38 -0400

    fixed typo

--- config/deploy.rb
+++ config/deploy.rb
@@ -37,7 +37,7 @@ namespace :deploy do
 
   task :setup_config, roles: :app do
    sudo "ln -nfs #{current_path}/deploy_configs/nginx.conf /etc/nginx/sites-enabled/#{application}"
-   sudo "#ln -nfs #{current_path}/deploy_configs/kapiqua_unicorn_init.sh /etc/init.d/unicorn_#{application}"
+   sudo "ln -nfs #{current_path}/deploy_configs/kapiqua_unicorn_init.sh /etc/init.d/unicorn_#{application}"
    run "mkdir -p #{shared_path}/config"
    put File.read("config/database.production.yml"), "#{shared_path}/config/database.yml"
    puts "Now edit the config files in #{shared_path}."
