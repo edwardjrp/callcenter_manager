d4df8ea1d9991ca53840e4b415d4747ac808b3fc
Radhames Brito <rbritom@gmail.com>
2012-07-19 22:13:43 -0400

    removed sesitive data

--- config/deploy.rb
+++ config/deploy.rb
@@ -31,13 +31,13 @@ namespace :deploy do
   %w[start stop restart].each do |command|
     desc "#{command} unicorn server"
     task command, roles: :app, except: {no_release: true} do
-      run "#{sudo} /etc/init.d/unicorn_#{application} #{command}"
+      sudo "/etc/init.d/unicorn_#{application} #{command}"
     end
   end
 
   task :setup_config, roles: :app do
-   run "#{sudo} ln -nfs #{current_path}/deploy_configs/nginx.conf /etc/nginx/sites-enabled/#{application}"
-   run "#{sudo} ln -nfs #{current_path}/deploy_configs/kapiqua_unicorn_init.sh /etc/init.d/unicorn_#{application}"
+   sudo "ln -nfs #{current_path}/deploy_configs/nginx.conf /etc/nginx/sites-enabled/#{application}"
+   sudo "#ln -nfs #{current_path}/deploy_configs/kapiqua_unicorn_init.sh /etc/init.d/unicorn_#{application}"
    run "mkdir -p #{shared_path}/config"
    put File.read("config/database.production.yml"), "#{shared_path}/config/database.yml"
    puts "Now edit the config files in #{shared_path}."
--- deploy_configs/kapiqua_unicorn_init.sh
+++ deploy_configs/kapiqua_unicorn_init.sh
@@ -7,7 +7,7 @@ set -e
 TIMEOUT=${TIMEOUT-60}
 APP_ROOT=/var/www/kapiqua25/current/public/current
 PID=$APP_ROOT/tmp/pids/unicorn.pid
-CMD="$APP_ROOT/bin/unicorn -D -c $APP_ROOT/config/unicorn.rb"
+CMD="$APP_ROOT/bin/unicorn -D -c $APP_ROOT/config/unicorn.rb -E production"
 action="$1"
 set -u
 
