5ac28968019b40a2fbb3e743d2d6eb23de4bfc12
Radhames Brito <rbritom@gmail.com>
2012-07-27 19:54:39 -0400

    about to pass cart_products

--- kapiquajs/dst/pulse_bridge/pulse_bridge.js
+++ kapiquajs/dst/pulse_bridge/pulse_bridge.js
@@ -7,10 +7,11 @@ request = require('request');
 libxml = require("libxmljs");
 
 PulseBridge = (function() {
+  var cart;
 
   function PulseBridge() {}
 
-  PulseBridge.debut = true;
+  PulseBridge.debut = false;
 
   PulseBridge.target = 'http://192.168.85.60:59101/RemotePulseAPI/RemotePulseAPI.WSDL';
 
@@ -22,6 +23,130 @@ PulseBridge = (function() {
     "Content-Type": "text/xml;charset=UTF-8"
   };
 
+  PulseBridge.cart = cart = {
+    "advance_order_time": null,
+    "business_date": null,
+    "can_place_order": null,
+    "client_id": null,
+    "communication_failed": false,
+    "company_name": null,
+    "completed": false,
+    "created_at": "2012-07-17T23:51:52-04:00",
+    "credit_cart_approval_name": null,
+    "delivery_instructions": null,
+    "discount": null,
+    "discount_auth_id": null,
+    "fiscal_number": null,
+    "fiscal_type": null,
+    "id": 1,
+    "message": null,
+    "net_amount": null,
+    "order_progress": null,
+    "order_text": null,
+    "payment_amount": null,
+    "payment_type": null,
+    "service_method": null,
+    "status_text": null,
+    "store_id": null,
+    "store_order_id": null,
+    "tax1_amount": null,
+    "tax2_amount": null,
+    "tax_amount": null,
+    "updated_at": "2012-07-17T23:51:52-04:00",
+    "user_id": 1,
+    "cart_products": [
+      {
+        "bind_id": 180,
+        "cart_id": 1,
+        "created_at": "2012-07-24T21:57:33-04:00",
+        "id": 80,
+        "options": "C-1,X,1.5C-2,G-2,M-2,O-2,R-2",
+        "product_id": 173,
+        "quantity": 3,
+        "updated_at": "2012-07-24T21:57:33-04:00",
+        "product": {
+          "category_id": 7,
+          "created_at": "2012-07-17T23:34:24-04:00",
+          "flavorcode": "DEEPDISH",
+          "id": 173,
+          "options": "X,C",
+          "optionselectiongrouptype": "PIZZA",
+          "productcode": "14PAN",
+          "productname": "14&quot; Gordita Napolitana",
+          "productoptionselectiongroup": "PIZZA",
+          "sizecode": "14",
+          "updated_at": "2012-07-17T23:34:24-04:00"
+        }
+      }, {
+        "bind_id": null,
+        "cart_id": 1,
+        "created_at": "2012-07-24T22:00:24-04:00",
+        "id": 81,
+        "options": "undefinedC-1,undefinedH-1,2X-1",
+        "product_id": 168,
+        "quantity": 1,
+        "updated_at": "2012-07-24T22:00:24-04:00",
+        "product": {
+          "category_id": 7,
+          "created_at": "2012-07-17T23:34:24-04:00",
+          "flavorcode": "DEEPDISH",
+          "id": 168,
+          "options": "X,1.5C,1.5H",
+          "optionselectiongrouptype": "PIZJX",
+          "productcode": "14FHDD",
+          "productname": "14&quot; Gordita Fiesta de Jamon",
+          "productoptionselectiongroup": "PIZJX",
+          "sizecode": "14",
+          "updated_at": "2012-07-17T23:34:24-04:00"
+        }
+      }, {
+        "bind_id": 179,
+        "cart_id": 1,
+        "created_at": "2012-07-24T22:00:27-04:00",
+        "id": 82,
+        "options": "1.5C-1,1.5H-1,X,C-2,1.5P-2",
+        "product_id": 168,
+        "quantity": 6,
+        "updated_at": "2012-07-24T22:00:34-04:00",
+        "product": {
+          "category_id": 7,
+          "created_at": "2012-07-17T23:34:24-04:00",
+          "flavorcode": "DEEPDISH",
+          "id": 168,
+          "options": "X,1.5C,1.5H",
+          "optionselectiongrouptype": "PIZJX",
+          "productcode": "14FHDD",
+          "productname": "14&quot; Gordita Fiesta de Jamon",
+          "productoptionselectiongroup": "PIZJX",
+          "sizecode": "14",
+          "updated_at": "2012-07-17T23:34:24-04:00"
+        }
+      }, {
+        "bind_id": null,
+        "cart_id": 1,
+        "created_at": "2012-07-24T13:04:13-04:00",
+        "id": 77,
+        "options": "2C-1,2X-1",
+        "product_id": 173,
+        "quantity": 4,
+        "updated_at": "2012-07-24T22:15:31-04:00",
+        "product": {
+          "category_id": 7,
+          "created_at": "2012-07-17T23:34:24-04:00",
+          "flavorcode": "DEEPDISH",
+          "id": 173,
+          "options": "X,C",
+          "optionselectiongrouptype": "PIZZA",
+          "productcode": "14PAN",
+          "productname": "14&quot; Gordita Napolitana",
+          "productoptionselectiongroup": "PIZZA",
+          "sizecode": "14",
+          "updated_at": "2012-07-17T23:34:24-04:00"
+        }
+      }
+    ]
+  };
+
   PulseBridge.make = function(action, data) {
     var doc;
     return doc = new libxml.Document();
@@ -29,12 +154,13 @@ PulseBridge = (function() {
 
   PulseBridge.send = function(action, data, err_cb, cb) {
     var body;
-    body = this.price_body().replace(/^\s*(.+)\s*$/g, '$1');
+    body = data;
     this.headers["Content-Length"] = body.length;
-    this.headers["SOAPAction"] = "http://www.dominos.com/action/PriceOrder";
-    console.log(this.headers);
-    console.log(body);
-    console.log(_.isString(body));
+    this.headers["SOAPAction"] = "http://www.dominos.com/action/" + action;
+    if (this.debug === true) {
+      console.log(this.headers);
+      console.log(body);
+    }
     return request.post({
       headers: this.headers,
       uri: this.target,
@@ -48,32 +174,12 @@ PulseBridge = (function() {
     });
   };
 
-  PulseBridge.envelope = function(doc) {
-    var envelope;
-    envelope = new libxml.Element(doc, 'env:Envelope').attr({
-      'xmlns:xsd': "http://www.w3.org/2001/XMLSchema",
-      'xmlns:xsi': "http://www.w3.org/2001/XMLSchema-instance",
-      'xmlns:wsdlns': "http://www.dominos.com/wsdl/",
-      'xmlns:env': 'http://schemas.xmlsoap.org/soap/envelope/',
-      'xmlns:ins0': 'http://www.dominos.com/type'
-    });
-    envelope.add(PulseBridge.pulse_header(doc));
-    return envelope;
+  PulseBridge.price = function(err_cb, cb) {
+    return PulseBridge.send('PriceOrder', PulseBridge.body('PriceOrder'), err_cb, cb);
   };
 
-  PulseBridge.pulse_header = function(doc) {
-    var auth, header;
-    auth = new libxml.Element(doc, 'Authorization');
-    auth.addChild(new libxml.Element(doc, 'FromURI', 'dominos.com'));
-    auth.addChild(new libxml.Element(doc, 'User', 'TestingAndSupport'));
-    auth.addChild(new libxml.Element(doc, 'Password', 'supp0rtivemeasures'));
-    auth.addChild(new libxml.Element(doc, 'TimeStamp', ''));
-    header = new libxml.Element(doc, 'env:Header').addChild(auth);
-    return header;
-  };
-
-  PulseBridge.price_body = function() {
-    var action, auth, body, cash_payment, customer, customer_address, customer_name, customer_type_info, doc, envelope, header, item_modifier, item_modifiers, order, order_item, order_items, order_source, payment;
+  PulseBridge.body = function(action, cart) {
+    var auth, body, cash_payment, customer, customer_address, customer_name, customer_type_info, doc, envelope, header, item_modifier, item_modifiers, order, order_item, order_items, order_source, payment;
     doc = new libxml.Document();
     envelope = new libxml.Element(doc, 'env:Envelope').attr({
       'xmlns:xsd': "http://www.w3.org/2001/XMLSchema",
@@ -188,7 +294,7 @@ PulseBridge = (function() {
     payment.addChild(cash_payment);
     order.addChild(payment);
     body = new libxml.Element(doc, 'env:Body');
-    action = new libxml.Element(doc, "ns1:PriceOrder").attr({
+    action = new libxml.Element(doc, "ns1:" + action).attr({
       'xmlns:ns1': "http://www.dominos.com/message/",
       encodingStyle: "http://schemas.xmlsoap.org/soap/encoding/"
     });
@@ -208,6 +314,8 @@ log = function(text) {
   return console.log(text);
 };
 
-PulseBridge.send('', 'data', log, log);
+PulseBridge.price(log, log);
+
+console.log(PulseBridge.cart);
 
 module.exports = PulseBridge;
--- kapiquajs/src/pulse_bridge/pulse_bridge.coffee
+++ kapiquajs/src/pulse_bridge/pulse_bridge.coffee
@@ -3,9 +3,11 @@ request = require('request')
 libxml = require("libxmljs")
 
 class PulseBridge
-  @debut = true
+  @debut = false
   @target = 'http://192.168.85.60:59101/RemotePulseAPI/RemotePulseAPI.WSDL'    
   @headers = {"User-Agent": "kapiqua-node" , "Connection": "close","Accept" : "text/html,application/xhtml+xml,application/xml","Accept-Charset": "utf-8", "Content-Type":"text/xml;charset=UTF-8"}
+  
+  @cart = cart = {"advance_order_time":null,"business_date":null,"can_place_order":null,"client_id":null,"communication_failed":false,"company_name":null,"completed":false,"created_at":"2012-07-17T23:51:52-04:00","credit_cart_approval_name":null,"delivery_instructions":null,"discount":null,"discount_auth_id":null,"fiscal_number":null,"fiscal_type":null,"id":1,"message":null,"net_amount":null,"order_progress":null,"order_text":null,"payment_amount":null,"payment_type":null,"service_method":null,"status_text":null,"store_id":null,"store_order_id":null,"tax1_amount":null,"tax2_amount":null,"tax_amount":null,"updated_at":"2012-07-17T23:51:52-04:00","user_id":1,"cart_products":[{"bind_id":180,"cart_id":1,"created_at":"2012-07-24T21:57:33-04:00","id":80,"options":"C-1,X,1.5C-2,G-2,M-2,O-2,R-2","product_id":173,"quantity":3,"updated_at":"2012-07-24T21:57:33-04:00","product":{"category_id":7,"created_at":"2012-07-17T23:34:24-04:00","flavorcode":"DEEPDISH","id":173,"options":"X,C","optionselectiongrouptype":"PIZZA","productcode":"14PAN","productname":"14&quot; Gordita Napolitana","productoptionselectiongroup":"PIZZA","sizecode":"14","updated_at":"2012-07-17T23:34:24-04:00"}},{"bind_id":null,"cart_id":1,"created_at":"2012-07-24T22:00:24-04:00","id":81,"options":"undefinedC-1,undefinedH-1,2X-1","product_id":168,"quantity":1,"updated_at":"2012-07-24T22:00:24-04:00","product":{"category_id":7,"created_at":"2012-07-17T23:34:24-04:00","flavorcode":"DEEPDISH","id":168,"options":"X,1.5C,1.5H","optionselectiongrouptype":"PIZJX","productcode":"14FHDD","productname":"14&quot; Gordita Fiesta de Jamon","productoptionselectiongroup":"PIZJX","sizecode":"14","updated_at":"2012-07-17T23:34:24-04:00"}},{"bind_id":179,"cart_id":1,"created_at":"2012-07-24T22:00:27-04:00","id":82,"options":"1.5C-1,1.5H-1,X,C-2,1.5P-2","product_id":168,"quantity":6,"updated_at":"2012-07-24T22:00:34-04:00","product":{"category_id":7,"created_at":"2012-07-17T23:34:24-04:00","flavorcode":"DEEPDISH","id":168,"options":"X,1.5C,1.5H","optionselectiongrouptype":"PIZJX","productcode":"14FHDD","productname":"14&quot; Gordita Fiesta de Jamon","productoptionselectiongroup":"PIZJX","sizecode":"14","updated_at":"2012-07-17T23:34:24-04:00"}},{"bind_id":null,"cart_id":1,"created_at":"2012-07-24T13:04:13-04:00","id":77,"options":"2C-1,2X-1","product_id":173,"quantity":4,"updated_at":"2012-07-24T22:15:31-04:00","product":{"category_id":7,"created_at":"2012-07-17T23:34:24-04:00","flavorcode":"DEEPDISH","id":173,"options":"X,C","optionselectiongrouptype":"PIZZA","productcode":"14PAN","productname":"14&quot; Gordita Napolitana","productoptionselectiongroup":"PIZZA","sizecode":"14","updated_at":"2012-07-17T23:34:24-04:00"}}]}
   
   @make: (action, data) ->
     doc = new libxml.Document()    
@@ -13,42 +15,25 @@ class PulseBridge
   @send: (action, data, err_cb, cb) ->
     # TestConnection
     # <Value>Hello there</Value>
-    body = @price_body().replace(/^\s*(.+)\s*$/g,'$1')
+    body = data
     # console.log body
     @headers["Content-Length"] =  body.length
-    @headers["SOAPAction"]= "http://www.dominos.com/action/PriceOrder"
-    # if @debug == true
-    console.log @headers
-    console.log body
-    console.log _.isString(body)
+    @headers["SOAPAction"]= "http://www.dominos.com/action/#{action}"
+    if @debug == true
+      console.log @headers
+      console.log body
     request.post {headers: @headers, uri: @target, body: body }, (err, res, res_data) ->
       if err
         err_cb(err)
       else
        cb(res_data)
-   
-   
-  @envelope: (doc) =>
-    envelope = new libxml.Element(doc,'env:Envelope').attr
-      'xmlns:xsd':"http://www.w3.org/2001/XMLSchema"
-      'xmlns:xsi':"http://www.w3.org/2001/XMLSchema-instance"
-      'xmlns:wsdlns':"http://www.dominos.com/wsdl/"
-      'xmlns:env':'http://schemas.xmlsoap.org/soap/envelope/'
-      'xmlns:ins0':'http://www.dominos.com/type'
-    envelope.add(@pulse_header(doc))
-    envelope
-     
-   
-  @pulse_header: (doc) =>
-    auth = new libxml.Element(doc,'Authorization')
-    auth.addChild(new libxml.Element(doc,'FromURI', 'dominos.com'))
-    auth.addChild(new libxml.Element(doc,'User', 'TestingAndSupport'))
-    auth.addChild(new libxml.Element(doc,'Password', 'supp0rtivemeasures'))
-    auth.addChild(new libxml.Element(doc,'TimeStamp',''))
-    header = new libxml.Element(doc,'env:Header').addChild(auth)
-    header
-      
-   @price_body: () =>
+  
+  
+  
+   @price: (err_cb, cb) ->
+     PulseBridge.send('PriceOrder', PulseBridge.body('PriceOrder'), err_cb, cb)    
+         
+   @body: (action, cart) =>
      doc = new libxml.Document()
      envelope = new libxml.Element(doc,'env:Envelope').attr
        'xmlns:xsd':"http://www.w3.org/2001/XMLSchema"
@@ -63,9 +48,6 @@ class PulseBridge
      auth.addChild(new libxml.Element(doc,'Password', 'supp0rtivemeasures'))
      auth.addChild(new libxml.Element(doc,'TimeStamp',''))
      header.addChild(auth)
-     
-     
-     
      
      order = new libxml.Element(doc,'Order').attr({orderid:"Order#1317916872", currency:"en-USD", language:"en-USA"})
      order.addChild(new libxml.Element(doc,'StoreID', '99998'))
@@ -149,7 +131,7 @@ class PulseBridge
      #end payment
      
      body = new libxml.Element(doc,'env:Body')
-     action = new libxml.Element(doc, "ns1:PriceOrder").attr({ 'xmlns:ns1':"http://www.dominos.com/message/", encodingStyle:"http://schemas.xmlsoap.org/soap/encoding/"})
+     action = new libxml.Element(doc, "ns1:#{action}").attr({ 'xmlns:ns1':"http://www.dominos.com/message/", encodingStyle:"http://schemas.xmlsoap.org/soap/encoding/"})
      action.addChild(order)
      body.addChild(action)
      envelope.addChild(header)
@@ -161,7 +143,8 @@ log = (text) ->
   console.log text
   
   
-PulseBridge.send('', 'data', log, log)
+PulseBridge.price(log, log)
+console.log PulseBridge.cart
 
 
 module.exports = PulseBridge
