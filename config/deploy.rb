c894c7532b224e832ca0da0f89f669ab5a77fae4
Radhames Brito <rbritom@gmail.com>
2012-09-18 19:44:50 -0400

    reorganize coffe deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -54,13 +54,13 @@ namespace :deploy do
   end
   before "deploy:setup_config", "deploy:compile_assets"
 
-  task :coffee_compile, roles: :app do
-    puts "Compiling coffee files."
-    run "cd #{current_path}/kapiquajs && npm install"
-    run "/usr/bin/coffee #{current_path}/kapiquajs/build.coffee"
-    puts "done."
-  end
-  before "deploy:symlink_config", "deploy:coffee_compile" 
+  # task :coffee_compile, roles: :app do
+  #   puts "Compiling coffee files."
+  #   run "cd #{current_path}/kapiquajs && npm install"
+  #   run "/usr/bin/coffee #{current_path}/kapiquajs/build.coffee"
+  #   puts "done."
+  # end
+  # before "deploy:symlink_config", "deploy:coffee_compile" 
 
   task :symlink_config, roles: :app do
     run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
