f10b2588cf77e0ea1f661c774fd8f533de9b5e77
Radhames Brito <rbritom@gmail.com>
2012-11-01 00:19:35 -0400

    quick fixes

--- app/models/user_cart.rb
+++ app/models/user_cart.rb
@@ -12,12 +12,14 @@
 class UserCart < ActiveRecord::Base
   belongs_to :user
   belongs_to :cart
+  validates :user_id, presence: true
+  validates :cart_id, presence: true
   attr_accessible :cart_id, :user_id
 
   def info
-    return if cart.nil?
-    return "id #{cart.id} - #{cart.client.last_phone.number.gsub(/([0-9]{3})([0-9]{3})([0-9]{4})/,'\\1-\\2-\\3')} - #{cart.client.last_phone.ext}" if  cart.client.present? && cart.client.last_phone.present? && cart.client.last_phone.ext.present?
-    return "id #{cart.id} - #{cart.client.last_phone.number.gsub(/([0-9]{3})([0-9]{3})([0-9]{4})/,'\\1-\\2-\\3')}" if  cart.client.present? && cart.client.last_phone.present?
-    "id #{cart.id}"
+    return if self.cart.nil?
+    return "id #{self.cart.id} - #{self.cart.client.last_phone.number.gsub(/([0-9]{3})([0-9]{3})([0-9]{4})/,'\\1-\\2-\\3')} - #{self.cart.client.last_phone.ext}" if  self.cart.client.present? && self.cart.client.last_phone.present? && self.cart.client.last_phone.ext.present?
+    return "id #{self.cart.id} - #{self.cart.client.last_phone.number.gsub(/([0-9]{3})([0-9]{3})([0-9]{4})/,'\\1-\\2-\\3')}" if  self.cart.client.present? && self.cart.client.last_phone.present?
+    "id #{self.cart.id}"
   end
 end
