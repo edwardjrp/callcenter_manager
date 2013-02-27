9c7823a61b105523f258124905c8f62c2813be92
Radhames Brito <rbritom@gmail.com>
2013-02-27 04:35:20 -0400

    pending buidlers heart fix

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,24 +1,46 @@
-set :host , '192.168.101.50'
+set :host , '10.0.0.116'
 server "#{host}", :web, :app, :db, primary: true
-set :user, 'soporte'
-set :repository,  "ssh://#{user}@#{host}/home/#{user}/#{application}.git"
-set :deploy_to, "/var/www/#{application}"
-set :bin_folder, "#{current_path}/bin"
+set :user, 'proteus'
+set :repository,  "ssh://#{user}@#{host}/Users/#{user}/#{application}.git"
+set :deploy_to, "/Library/WebServer/#{application}"
+# set :bin_folder, "#{current_path}/bin"
+set :templates_path, "config/recipes/templates"
 
 
 namespace :deploy do
-  %w[start stop restart].each do |command|
-    desc "#{command} unicorn server"
-    task command, roles: :app, except: {no_release: true} do
-      sudo "/etc/init.d/unicorn_#{application} #{command}"
-    end
+  task :restart, :roles => :app do
+    run "touch #{current_release}/tmp/restart.txt"
   end
 
   # task :setup_config, roles: :app do
-  #  sudo "ln -nfs #{current_path}/deploy_configs/kapiqua.conf /etc/nginx/sites-enabled/#{application}"
+   # sudo "ln -nfs #{current_path}/deploy_configs/kapiqua.conf /usr/local/etc/nginx/nginx.conf"
   #  sudo "ln -nfs #{current_path}/deploy_configs/kapiqua_unicorn_init.sh /etc/init.d/unicorn_#{application}"
   #  sudo "chmod +x #{current_path}/deploy_configs/kapiqua_unicorn_init.sh"
   #  run "mkdir -p #{shared_path}/config"
+  #  run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
   # end
   # before "deploy:restart", "deploy:setup_config"
+end
+
+
+namespace :unicorn do
+  desc "generate unicorn setup"
+  task :setup, roles: :app do
+    erb_plist = File.read(File.expand_path("#{templates_path}/unicorn_start_osx.erb"))
+    result_plist = ERB.new(erb_plist).result(binding)
+    puts result_plist
+    # put result_plist, '/tmp/unicorn_start_osx.plist'
+    # run "#{sudo} mv /tmp/unicorn_start_osx.conf /Library/LaunchDaemons/unicorn.plist"
+  end
+
+  desc "generate unicorn config"
+  task :config, roles: :app do
+    erb_config = File.read(File.expand_path("#{templates_path}/unicorn_config.erb"))
+    result_config = ERB.new(erb_config).result(binding)
+    puts result_config
+    # put result_config, '/tmp/unicorn_config.conf'
+    # run "#{sudo} mv /tmp/unicorn_config.conf #{current_path}/config/unicorn.rb"
+  end
+
+
 end
