ebed22664cddf9ef5bdd1850d689ab50fbc9f63d
Radhames Brito <rbritom@gmail.com>
2013-03-09 17:59:34 -0400

    asterisk connector for per hour calls set

--- lib/asterisk/connector.rb
+++ lib/asterisk/connector.rb
@@ -19,14 +19,24 @@ module Asterisk
     end
 
     def total_incoming
-      result = JSON.parse(Net::HTTP.get(URI.parse(end_point)))
+      result = JSON.parse(Net::HTTP.get(URI.parse("#{end_point}/totalincoming.json?fecha1=#{@start_time.to_date}&fecha2=#{@end_time.to_date}&token=#{token(nonce)}&nonce=#{nonce}")))
       result["result"]["totalincoming"] if result and result["result"]
+    end
+
+    def calls_by_hour
+      result = JSON.parse(Net::HTTP.get(URI.parse("#{end_point}/callsbyhour.json?fecha1=#{@start_time.to_date}&fecha2=#{@end_time.to_date}&token=#{token(nonce)}&nonce=#{nonce}")))
+      result["result"] if result and result["result"]
+    end
+
+    def agents_by_hour
+      result = JSON.parse(Net::HTTP.get(URI.parse("#{end_point}/agentsbyhour.json?fecha1=#{@start_time.to_date}&fecha2=#{@end_time.to_date}&token=#{token(nonce)}&nonce=#{nonce}")))
+      result["result"] if result and result["result"]
     end
 
     private 
 
     def nonce
-      SecureRandom.hex(10)
+      @nonce ||= SecureRandom.hex(10)
     end
   end
 end
--- spec/lib/asterisk/connector_spec.rb
+++ spec/lib/asterisk/connector_spec.rb
@@ -27,10 +27,100 @@ describe Asterisk::Connector do
     Asterisk::Connector.new.token(nonce_key).should == token_result
   end
 
-  it 'should return the connection token' do
-    Net::HTTP.stub(:get).and_return({"resultcode"=>0, "result"=>{"totalincoming"=>16573}}.to_json)
-    Asterisk::Connector.new(Date.current.yesterday.to_time, Time.now).total_incoming.should == 16573
+  describe '#total_incoming' do
+    it 'should return the total incoming calls for a given date range' do
+      Net::HTTP.stub(:get).and_return({"resultcode"=>0, "result"=>{"totalincoming"=>16573}}.to_json)
+      Asterisk::Connector.new(Date.current.yesterday.to_time, Time.now).total_incoming.should == 16573
+    end
   end
 
+  describe '#calls_by_hour' do
+    let!(:calls_by_hour_hash) do
+      {
+        "resultcode"=>0,
+        "result"=>{
+          "2013-03-06"=>{
+            "0"=>"1",
+            "1"=>"2",
+            "7"=>"2",
+            "10"=>"9",
+            "11"=>"34",
+            "12"=>"42",
+            "13"=>"33",
+            "14"=>"26",
+            "15"=>"22",
+            "16"=>"23",
+            "17"=>"41",
+            "18"=>"46",
+            "19"=>"72",
+            "20"=>"58",
+            "21"=>"49",
+            "22"=>"23",
+            "23"=>"3"
+          },
+          "2013-03-07"=>{
+            "10"=>"23",
+            "11"=>"35",
+            "12"=>"87",
+            "13"=>"72",
+            "14"=>"11"
+          }
+        }
+      }
+    end
 
+    before { Net::HTTP.stub(:get).and_return(calls_by_hour_hash.to_json) }
+
+    it 'should return a hash of calls per day per hour' do
+      Asterisk::Connector.new(Date.current.yesterday.to_time, Time.now).calls_by_hour["2013-03-07"].should == { "10"=>"23","11"=>"35","12"=>"87","13"=>"72","14"=>"11"}
+    end
+  end
+
+  describe '#agents_by_hour' do
+    let!(:agents_by_hour_hash) do
+      {
+        "resultcode"=>0,
+        "result"=>{
+          "2013-03-06"=>{
+            "0"=>"1.0",
+            "1"=>"1.0",
+            "2"=>"1.0",
+            "3"=>"1.0",
+            "4"=>"1.0",
+            "5"=>"1.0", 
+            "6"=>"1.0",
+            "7"=>"1.0",
+            "8"=>"1.0",
+            "10"=>"2.0",
+            "11"=>"3.75",
+            "12"=>"4.75",
+            "13"=>"5.0",
+            "14"=>"4.75",
+            "15"=>"5.0",
+            "16"=>"2.75",
+            "17"=>"6.0",
+            "18"=>"8.75",
+            "19"=>"9.5",
+            "20"=>"10.0",
+            "21"=>"10.0",
+            "22"=>"3.5",
+            "23"=>"1.0"
+          },
+          "2013-03-07"=>{
+            "10"=>"4.3333",
+            "11"=>"6.5",
+            "12"=>"8.75",
+            "13"=>"9.0",
+            "14"=>"9.0"
+          }
+        }
+      }
+    end
+
+    before { Net::HTTP.stub(:get).and_return(agents_by_hour_hash.to_json) }
+
+    it 'should return a hash connected agents per day per hour' do
+      Asterisk::Connector.new(Date.current.yesterday.to_time, Time.now).agents_by_hour["2013-03-07"].should == {"10"=>"4.3333","11"=>"6.5","12"=>"8.75","13"=>"9.0","14"=>"9.0"}
+    end
+  end
 end
