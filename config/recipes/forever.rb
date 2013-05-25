6e4b8a79cecb297be6c3ae45fcfc6f59158c5dac
Radhames Brito <rbritom@gmail.com>
2013-05-25 08:04:41 -0400

    udpated recipe to ignore node restart fail

--- config/recipes/forever.rb
+++ config/recipes/forever.rb
@@ -6,20 +6,20 @@ namespace :forever do
 
 
   desc "start forever"
-  task :start, roles: :web do
+  task :start, roles: :web, on_error: :continue do
     run "#{sudo} /bin/bash -c '#{current_path}/deploy_configs/forever start'"
   end
 
 
 
   desc "stop forever"
-  task :stop, roles: :web do
+  task :stop, roles: :web, on_error: :continue do
     run "#{sudo} /bin/bash -c '#{current_path}/deploy_configs/forever stopall'"
   end
 
 
   desc "restart forever"
-  task :restart,roles: :web do
+  task :restart,roles: :web, on_error: :continue do
     stop
     start
   end
